ee00a669ee8e08a2346145c9ceb8e1cd495834af
ramkarthikn <44068458+ramkarthikn@users.noreply.github.com>
2025-07-15 23:12:09 +0530

    Update tls_test.sh

--- tls_test.sh
+++ tls_test.sh
@@ -2,3 +2,6 @@ openssl s_client -connect <I.P> -servername <Domain-name> -tls1_3 </dev/null 2>&
 openssl s_client -connect <I.P> -servername <Domain-name> -tls1_2 </dev/null 2>&1 | grep -E "Protocol|Cipher"
 openssl s_client -connect <I.P> -servername <Domain-name> -tls1_1 </dev/null 2>&1 | grep -E "Protocol|Cipher"
 openssl s_client -connect <I.P> -servername <Domain-name> -tls1 </dev/null 2>&1 | grep -E "Protocol|Cipher"
+
+#openssl s_client -connect <I.P>:<PORT> -servername <Domain-name> <TLS_VERSION_FLAG> -cipher <CIPHER_SUITE_NAME> -showcerts </dev/null 2>&1 | grep -E "Protocol|Cipher" 
+# For checking specific cipher
